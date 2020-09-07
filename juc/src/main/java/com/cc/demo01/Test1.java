d53c88b73e6c2e3ce95c62a6c72a5c497ecf2996
chengcheng1021 <chengcheng1021@aliyun.com>
2020-09-07 15:35:19 +0800

    线程和进程

---
+++ juc/src/main/java/com/cc/demo01/Test1.java
@@ -0,0 +1,10 @@
+package com.cc.demo01;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        //获取cpu核数
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
